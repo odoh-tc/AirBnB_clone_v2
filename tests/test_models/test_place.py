53b35ed633e28fcc177d45f1b9cb02f855e6a855
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2023-09-19 10:51:20 +0000

    tests

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -18,45 +18,45 @@ class test_Place(test_basemodel):
         new = self.value()
         self.assertEqual(type(new.city_id), str)
 
-    def test_user_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
-
     def test_name(self):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+    def test_user_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.user_id), str)
 
     def test_description(self):
         """ """
         new = self.value()
         self.assertEqual(type(new.description), str)
 
-    def test_number_rooms(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.number_rooms), int)
-
     def test_number_bathrooms(self):
         """ """
         new = self.value()
         self.assertEqual(type(new.number_bathrooms), int)
+
+    def test_number_rooms(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.number_rooms), int)
 
     def test_max_guest(self):
         """ """
         new = self.value()
         self.assertEqual(type(new.max_guest), int)
 
-    def test_price_by_night(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.price_by_night), int)
-
     def test_latitude(self):
         """ """
         new = self.value()
         self.assertEqual(type(new.latitude), float)
+
+    def test_price_by_night(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.price_by_night), int)
 
     def test_longitude(self):
         """ """
@@ -67,3 +67,4 @@ class test_Place(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.amenity_ids), list)
+
